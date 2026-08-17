2ddbb152244e5bfd18254edf722a0d8395341eae
BenjaminD <benjamin.denis.i2d@gmail.com>
2026-08-17 23:20:59 +0200

    clear console log

--- src/app.module.ts
+++ src/app.module.ts
@@ -61,11 +61,7 @@ export class AppModule implements OnModuleInit {
   async onModuleInit() {
     
     const userRepository = this.dataSource.getRepository(User); 
-
-    console.log(process.env.POSTGRES_HOST);
-    console.log(process.env.POSTGRES_PORT);
-    console.log(process.env.POSTGRES_USER);
-
+    
     await UserSeed.run(userRepository);
   }
 
--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,5 +1,3 @@
-console.log('JWT_SECRET:', process.env.JWT_SECRET);
-
 export const jwtConstants = {
     secret: process.env.JWT_SECRET,
   };
